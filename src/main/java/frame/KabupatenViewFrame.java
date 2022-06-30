edf165ad2b06c46666e917aa56070728d4a74b6a
62813 <nuraliahifji@gmail.com>
2022-06-30 23:06:26 +0800

    Read, Search, Delete pada KabupatenViewFrame

--- src/main/java/frame/KabupatenViewFrame.java
+++ src/main/java/frame/KabupatenViewFrame.java
@@ -1,6 +1,14 @@
 package frame;
 
+import helpers.Koneksi;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.xml.transform.Result;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.*;
 
 public class KabupatenViewFrame extends JFrame {
     private JPanel mainPanel;
@@ -17,9 +25,7 @@ public class KabupatenViewFrame extends JFrame {
     private JButton cetakButton;
     private JButton tutupButton;
 
-    public KabupatenViewFrame(){
-        init();
-    }
+
     public void init(){
         setContentPane(mainPanel);
         setTitle("Data Kabupaten");
@@ -27,5 +33,91 @@ public class KabupatenViewFrame extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
     }
+
+    public void isiTable(){
+        Connection c = Koneksi.getConnection();
+        String selectSQL = "SELECT * FROM kabupaten";
+        try {
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery(selectSQL);
+            String header[] = {"Id", "Nama Kabupaten"};
+            DefaultTableModel dtm = new DefaultTableModel(header, 0);
+            viewTable.setModel(dtm);
+            Object[] row = new Object[2];
+            while (rs.next()){
+                row[0] = rs.getInt("id");
+                row[1] = rs.getString("nama");
+                dtm.addRow(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+    public KabupatenViewFrame(){
+        tutupButton.addActionListener(e ->{
+            dispose();
+        });
+        batalButton.addActionListener(e ->{
+            isiTable();
+        });
+        addWindowFocusListener(new WindowAdapter() {
+            @Override
+            public void windowActivated(WindowEvent e){
+                isiTable();
+            }
+        });
+        isiTable();
+        init();
+
+
+        cariButton.addActionListener(e ->{
+            Connection c = Koneksi.getConnection();
+            String keyword = "%" + cariTextField.getText() + "%";
+            String searchSQL = "SELECT * FROM kabupaten WHERE nama like ?";
+            try {
+                PreparedStatement ps = c.prepareStatement(searchSQL);
+                ps.setString(1, keyword);
+                ResultSet rs = ps.executeQuery();
+                DefaultTableModel dtm = (DefaultTableModel) viewTable.getModel();
+                dtm.setRowCount(0);
+                Object[] row = new Object[2];
+                while (rs.next()){
+                    row[0] = rs.getInt("id");
+                    row[1] = rs.getString("nama");
+                    dtm.addRow(row);
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+
+        hapusButton.addActionListener(e ->{
+            int barisTerpilih = viewTable.getSelectedRow();
+            if(barisTerpilih < 0){
+                JOptionPane.showConfirmDialog(null, "Pilih data dulu");
+                return;
+            }
+            int pilihan = JOptionPane.showConfirmDialog(null. "Yakin mau hapus?", "Konfirmasi Hapus", JOptionPane.YES_NO_OPTION);
+            if (pilihan == 0){
+                TableModel tm = viewTable.getModel();
+                int id = Integer.parseInt(tm.getValueAt(barisTerpilih,0).toString());
+                Connection c = Koneksi.getConnection();
+                String deleteSQL = "DELETE FROM kabupaten WHERE id = ?";
+                try {
+                    PreparedStatement ps = c.prepareStatement(deleteSQL);
+                    ps.setInt(1,id);
+                    ps.executeUpdate();
+                } catch (SQLException ex){
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+
 
 }
